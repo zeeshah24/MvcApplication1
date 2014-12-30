54f7d2d65f082c38bf77f226c984c80f7478ec9d
zeeshah24 <zeeshah24@gmail.com>
2014-12-30 12:11:19 +0800

    MVC_Demo
    MVC_Demo

---
+++ MvcApplication1/Controllers/DemoController.cs
@@ -0,0 +1,47 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class DemoController : Controller
+    {
+        //
+        // GET: /Demo/
+
+        public String Index()
+        {
+            return "Just to call default action.";
+        }
+        public ActionResult CallStudent()
+        {
+            Student shah = new Student();
+            shah.StudentID = 1;
+            shah.FirstName = "Shah";
+            shah.LastName = "Khan";
+            return View(shah);
+        }
+        public string MyCall(int id)
+        {
+            Student student = new Student();
+            if (id == 1)
+            {
+                student.StudentID = 1;
+                student.FirstName = "Shah";
+                student.LastName = "Khan";
+            }
+            else
+            {
+
+                //Student naren = new Student();
+                student.StudentID = 2;
+                student.FirstName = "Narendra";
+                student.LastName = "Dagwar";
+            }
+            return "This method is called from DemoController MyCall function";
+        }
+    }
+}
---
+++ MvcApplication1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
